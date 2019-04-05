69e24e5e6f4449269b908a444c9ff39b44ea6d7d
Josh Akeman <joshakeman@gmail.com>
2019-04-04 22:18:56 -0400

    individual friends rendering and update/delete buttons rendered

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import '../App.css'
 
 const Friend = props => {
     const friend = props.friends.find(friend => `${friend.id}` === props.match.params.friendId)
     if (!friend) return <h3>Loading data...</h3>;
     return (
-        <div className="friend-card">
-        <h1>{friend.name} </h1>
-        <p>{friend.age}</p>
-        <p>{friend.email}</p>
+        <div className="friend-card-wrapper">
+            <div className="friend-card">
+                <h1>{friend.name} </h1>
+                <p>{friend.age}</p>
+                <p>{friend.email}</p>
+                <button className="other-btn grn">Update Friend</button>
+                <button className="other-btn red">Destroy Friend</button>
+            </div>
+            
         </div>
     )
 }
